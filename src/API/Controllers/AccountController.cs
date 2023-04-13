ebaa9a2e73ee9d278455277ad4b17679985ae7f9
Nkosana <nkosana.prince.ndlela@gmail.com>
2023-04-13 17:42:11 +0200

    check in

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -77,7 +77,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            //var user = await _userManager.Users  
+            //var user = await _userManager.Users    
             //    .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
             var user = await _userManager.Users.Include(p => p.UserPhotos)
