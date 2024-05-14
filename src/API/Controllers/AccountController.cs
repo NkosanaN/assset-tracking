d3243a8e3b258fdfe3e170ea099339a78702522d
nkosana ndlela <nkosana.prince.ndlela@gmail.com>
2024-05-14 18:06:30 +0200

    clean-up

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -6,97 +6,96 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-namespace API.Controllers
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(
+    UserManager<AppUser> userManager,
+    TokenService tokenService) : ControllerBase
 {
+    private readonly UserManager<AppUser> _userManager = userManager;
+    private readonly TokenService _tokenService = tokenService;
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController(
-        UserManager<AppUser> userManager,
-        TokenService tokenService) : ControllerBase
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto login)
     {
-        private readonly UserManager<AppUser> _userManager = userManager;
-        private readonly TokenService _tokenService = tokenService;
+        //var user = await _userManager.FindByEmailAsync(login.Email);
+        var user = await _userManager.Users.Include(p =>
+            p.UserPhotos).FirstOrDefaultAsync(x => x.Email == login.Email);
 
-        [AllowAnonymous]
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto login)
+        if (user == null) return Unauthorized();
+
+        var result = await _userManager.CheckPasswordAsync(user, login.Password);
+
+        if (result)
         {
-            //var user = await _userManager.FindByEmailAsync(login.Email);
-            var user = await _userManager.Users.Include(p =>
-                p.UserPhotos).FirstOrDefaultAsync(x => x.Email == login.Email);
-
-            if (user == null) return Unauthorized();
-
-            var result = await _userManager.CheckPasswordAsync(user, login.Password);
-
-            if (result)
-            {
-                return CreateUserObject(user);
-            }
-
-            return Unauthorized();
-        }
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
-            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
-            {
-                ModelState.AddModelError("username", "Username taken");
-                return BadRequest("Username taken");
-            }
-            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
-            {
-                ModelState.AddModelError("Email", "Email taken");
-                return BadRequest("Email taken");
-            }
-
-            var user = new AppUser
-            {
-                Firstname = registerDto.Firstname,
-                Lastname = registerDto.Lastname,
-                DisplayName = registerDto.DisplayName,
-                Email = registerDto.Email,
-                UserName = registerDto.Username,
-                AddressLine1 = registerDto.AddressLine1,
-            };
-
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
-
-            if (result.Succeeded)
-            {
-                return CreateUserObject(user);
-            }
-
-            return BadRequest(result.Errors);
-        }
-        [Authorize]
-        [HttpGet]
-        public async Task<ActionResult<UserDto>> GetCurrentUser()
-        {
-            //var user = await _userManager.Users    
-            //    .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-
-            var user = await _userManager.Users.Include(p => p.UserPhotos)
-                .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-
-            return CreateUserObject(user!);
-        }
-        //[HttpPost]
-        //public async Task<ActionResult> Logout()
-        //{
-        //    await _signInManager.SignOutAsync(); // Sign out the user
-
-        //    // Return a success message
-        //    return Ok(new { message = "Logout successful" });
-        //}
-
-        private UserDto CreateUserObject(AppUser user)
-        {
-            return new UserDto(user.DisplayName, _tokenService.CreateToken(user!), string.Empty, user!.UserName!, user.Firstname, user.Lastname);
+            return CreateUserObject(user);
         }
 
+        return Unauthorized();
+    }
 
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+        {
+            ModelState.AddModelError("username", "Username taken");
+            return BadRequest("Username taken");
+        }
+
+        if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+        {
+            ModelState.AddModelError("Email", "Email taken");
+            return BadRequest("Email taken");
+        }
+
+        var user = new AppUser
+        {
+            Firstname = registerDto.Firstname,
+            Lastname = registerDto.Lastname,
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.Username,
+            AddressLine1 = registerDto.AddressLine1,
+        };
+
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (result.Succeeded)
+        {
+            return CreateUserObject(user);
+        }
+
+        return BadRequest(result.Errors);
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        //var user = await _userManager.Users    
+        //    .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+        var user = await _userManager.Users.Include(p => p.UserPhotos)
+            .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+        return CreateUserObject(user!);
+    }
+    //[HttpPost]
+    //public async Task<ActionResult> Logout()
+    //{
+    //    await _signInManager.SignOutAsync(); // Sign out the user
+
+    //    // Return a success message
+    //    return Ok(new { message = "Logout successful" });
+    //}
+
+    private UserDto CreateUserObject(AppUser user)
+    {
+        return new UserDto(user.DisplayName, _tokenService.CreateToken(user!), string.Empty, user!.UserName!, user.Firstname, user.Lastname);
     }
 }
--- src/API/Controllers/BaseApiController.cs
+++ src/API/Controllers/BaseApiController.cs
@@ -3,56 +3,53 @@ using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseApiController : ControllerBase
-    {
-        private IMediator? _mediator;
+namespace API.Controllers;
 
-        /*
-         * First check whether we've Mediator which is active if not request new Mediator
-         */
+[Route("api/[controller]")]
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+    private IMediator? _mediator;
+
+    /*
+     * First check whether we've Mediator which is active if not request new Mediator
+     */
 #pragma warning disable CS8603 // Possible null reference return.
-        protected IMediator Mediator => _mediator ??=
-            HttpContext.RequestServices.GetService<IMediator>();
+    protected IMediator Mediator => _mediator ??=
+        HttpContext.RequestServices.GetService<IMediator>();
 #pragma warning restore CS8603 // Possible null reference return.
 
-        protected IActionResult HandlerResult<T>(Result<T> result)
+    protected IActionResult HandlerResult<T>(Result<T> result)
+    {
+        if (result is null) return NotFound();
+
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);
+
+        if (result!.IsSuccess && result.Value == null)
+            return NotFound();
+
+        return BadRequest(result.Error);
+    }
+
+    protected IActionResult HandlerPagedResult<T>(Result<PagedList<T>> result)
+    {
+        if (result is null) return NotFound();
+
+        if (result.IsSuccess && result.Value != null)
         {
-            if (result is null) return NotFound();
-
-            if (result.IsSuccess && result.Value != null)
-                return Ok(result.Value);
-
-            if (result!.IsSuccess && result.Value == null)
-                return NotFound();
-
-            return BadRequest(result.Error);
-
+            Response.AddPaginationHeader(
+                result.Value.CurrentPage,
+                result.Value.PageSize,
+                result.Value.TotalCount,
+                result.Value.TotalPages);
+         
+            return Ok(result.Value);
         }
 
-        protected IActionResult HandlerPagedResult<T>(Result<PagedList<T>> result)
-        {
-            if (result is null) return NotFound();
+        if (result!.IsSuccess && result.Value == null)
+            return NotFound();
 
-            if (result.IsSuccess && result.Value != null)
-            {
-                Response.AddPaginationHeader(
-                    result.Value.CurrentPage,
-                    result.Value.PageSize,
-                    result.Value.TotalCount,
-                    result.Value.TotalPages);
-             
-                return Ok(result.Value);
-            }
-
-            if (result!.IsSuccess && result.Value == null)
-                return NotFound();
-
-            return BadRequest(result.Error);
-
-        }
+        return BadRequest(result.Error);
     }
 }
